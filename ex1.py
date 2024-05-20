56e349653ebbe0c0a79197b3f86e5915a489950d
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2024-05-20 15:48:30 +0300

    with plot

--- ex1.py
+++ ex1.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 class CSAutomata:
@@ -9,13 +10,15 @@ class CSAutomata:
         self.interval_entry = None
         self.size_entry = None
         self.master = master
-        self.size = 40
+        self.size = 80
         self.cell_size = 15
         self.interval = 10
         self.iterations = 0
+        self.max_iterations = 250
         self.running = False
         self.result_data = []
         self.run_count = 0
+        self.total_runs = 10
         self.all_results = []
 
         self.setup_menu()
@@ -32,7 +35,7 @@ class CSAutomata:
         size_label = tk.Label(menu_frame, text="Grid Size:")
         size_label.pack(side=tk.LEFT)
         self.size_entry = tk.Entry(menu_frame)
-        self.size_entry.insert(0, "40")
+        self.size_entry.insert(0, "80")
         self.size_entry.pack(side=tk.LEFT)
 
         interval_label = tk.Label(menu_frame, text="Interval (ms):")
@@ -63,6 +66,7 @@ class CSAutomata:
             self.grid[row][col] = 1
 
         self.iterations = 0
+        self.result_data = []
 
     def draw_grid(self):
         self.canvas.delete("all")
@@ -85,7 +89,7 @@ class CSAutomata:
 
         self.canvas.config(width=self.size * self.cell_size, height=self.size * self.cell_size)
         self.running = True
-        self.run_count = 10
+        self.run_count = 0
         self.all_results = []
         self.run()
 
@@ -100,48 +104,24 @@ class CSAutomata:
         self.run_count = 0
 
     def run(self):
-        if self.running:
-            self.iterations += 1
-            self.update_grid()
-            self.draw_grid()
-            self.master.after(self.interval, self.run)
-            print(f"Iteration: {self.iterations}")
+        if self.running and self.run_count < self.total_runs:
+            if self.iterations < self.max_iterations:
+                self.iterations += 1
+                self.update_grid()
+                self.draw_grid()
+                self.calculate_result()
+                self.master.after(self.interval, self.run)
+                print(f"Run {self.run_count + 1}, Iteration {self.iterations}, Result: {self.result_data[-1]}")
+            else:
+                self.all_results.append(self.result_data)
+                self.run_count += 1
+                self.initialize_grid()
+                self.iterations = 0
+                self.run()
+        elif self.run_count == self.total_runs:
+            self.stop_game()
+            self.plot_result()
 
-    # def update_grid(self):
-    #     new_grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
-    #     for i in range(self.size):
-    #         for j in range(self.size):
-    #             me = self.grid[i][j]
-    #             # Column rule
-    #             top = self.grid[(i - 1) % self.size][j]
-    #             low = self.grid[(i + 1) % self.size][j]
-    #             # Row rule
-    #             left = self.grid[i][(j - 1) % self.size]
-    #             right = self.grid[i][(j + 1) % self.size]
-    #             # Diagonals top
-    #             top_left = self.grid[(i - 1) % self.size][(j - 1) % self.size]
-    #             top_right = self.grid[(i - 1) % self.size][(j + 1) % self.size]
-    #             # Diagonals bottom
-    #             low_right = self.grid[(i + 1) % self.size][(j + 1) % self.size]
-    #             low_left = self.grid[(i + 1) % self.size][(j - 1) % self.size]
-    #             right_sum = right + low_right + top_right
-    #             left_sum = left + top_left + low_left
-    #             middle_sum = left + right + me
-    #             top_sum = top + top_left + low_left
-    #             low_sum = low + low_left + low_right
-    #             if top == 0 and top_left == top_right == 1:
-    #                 new_grid[i][j] = 0
-    #             elif right_sum + left_sum >= 4:
-    #                 new_grid[i][j] = 0
-    #             elif right_sum + left_sum == 1 or left_sum + right_sum == 2:
-    #                 new_grid[i][j] = 1
-    #             elif top_sum + middle_sum + low_sum == 1:
-    #                 new_grid[i][j] = 1
-    #             else:
-    #                 new_grid[i][j] = self.grid[i][j]
-    #     self.grid = new_grid
-
-    # ORIGINAL
     def update_grid(self):
         new_grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
         for i in range(self.size):
@@ -161,14 +141,12 @@ class CSAutomata:
                 low_left = self.grid[(i + 1) % self.size][(j - 1) % self.size]
                 right_sum = right + low_right + top_right
                 left_sum = left + top_left + low_left
-                if top == 0 and top_left == 1 and top_right == 1:
-                    new_grid[i][j] = 0
+                if low == 1 and low_left == 0 and low_right == 0:
+                    new_grid[i][j] = 1
                 elif right_sum + left_sum >= 4:
                     new_grid[i][j] = 0
                 elif right_sum + left_sum <= 2:
                     new_grid[i][j] = 1
-                elif low == 0 and low_left == 1 and low_right == 1:
-                    new_grid[i][j] = 0
                 else:
                     new_grid[i][j] = self.grid[i][j]
         self.grid = new_grid
@@ -183,13 +161,13 @@ class CSAutomata:
                 Ri_values.append(col_sum / self.size)
         current_result = sum(Ri_values) / len(Ri_values)
         self.result_data.append(current_result)
-        print("current accuracy:" + current_result)
 
     def plot_result(self):
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(10, 6))
+        colors = cm.rainbow([i / self.total_runs for i in range(self.total_runs)])
         for idx, result_data in enumerate(self.all_results):
             plt.plot(range(1, len(result_data) + 1), result_data, marker='o', linestyle='-', markersize=3,
-                     label=f"Run {idx + 1}")
+                     color=colors[idx], label=f"Run {idx + 1}")
         plt.title('Average Ri Over Multiple Runs')
         plt.xlabel('Iterations')
         plt.ylabel('Average Ri')
